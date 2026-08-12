047bc484d0151981e29dbe5d43ee70bf70847f09
Jack <jackstar6677@users.noreply.github.com>
2026-08-12 09:26:58 -0400

    fix: sanea descriptor y empaquetado para Java 21

--- src/main/java/io/github/sefiraat/equivalencytech/recipes/Recipes.java
+++ src/main/java/io/github/sefiraat/equivalencytech/recipes/Recipes.java
@@ -1,6 +1,6 @@
 package io.github.sefiraat.equivalencytech.recipes;
 
-import io.github.sefiraat.equivalencytech.EquivalencyTech;;
+import io.github.sefiraat.equivalencytech.EquivalencyTech;
 import com.github.drakescraft_labs.slimefun4.api.items.SlimefunItemStack;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
